710cb8fe3c4725b94cc23fff56fbc36faef3bcf8
Mark Melnic <markhappy200@gmail.com>
2020-11-03 17:49:12 +0100

    Sync, database and stopping procedure improvements

--- crawler/runner.py
+++ crawler/runner.py
@@ -6,20 +6,17 @@ from crawler.visualizer import live_graph
 
 
 class CRAWLER:
-    def __init__(self, db, graph=False):
+    def __init__(self, database, graph=False):
+        self.db = database
         self.running = True
 
         # initialize arrays
-        self.active_links = db.read_table("active_links")
-        self.listings_links = db.read_table("listings_links")
-        self.processed_links = db.read_table("processed_links")
+        self.active_links = self.db.read_table("active_links")
+        self.listings_links = self.db.read_table("listings_links")
+        self.processed_links = self.db.read_table("processed_links")
 
-        # start the database updater thread
-        self.db_thread = Thread(target=self.database_updater, args=(db,))
-        self.db_thread.start()
-
+        # start the live graph in a separate thread
         if graph:
-            # start the live graph in a separate thread
             graph_thread = Thread(target=live_graph, args=(self,))
             graph_thread.start()
 
@@ -35,12 +32,10 @@ class CRAWLER:
             array.pop(0)
 
     # database updater thread
-    def database_updater(self, db) -> None:
-        while self.running:
-            time.sleep(60)
-            db.rewrite_table_values("active_links", self.active_links)
-            db.rewrite_table_values("listings_links", self.listings_links)
-            db.rewrite_table_values("processed_links", self.processed_links)
+    def db_sync_links(self) -> None:
+        self.db.rewrite_table_values("active_links", self.active_links)
+        self.db.rewrite_table_values("listings_links", self.listings_links)
+        self.db.rewrite_table_values("processed_links", self.processed_links)
 
     # return first listing
     def listing(self):
@@ -49,6 +44,5 @@ class CRAWLER:
     # stop crawler execution
     def stop(self):
         self.running = False
+        self.db_sync_links()
         self.mde_crawler_thread.join()
-        self.db_thread.join()
-        # os._exit(0)
