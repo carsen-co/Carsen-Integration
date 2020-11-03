758b7f35453778ecd64b423fc9843bfc64468fbb
Mark Melnic <markhappy200@gmail.com>
2020-11-03 13:11:58 +0100

    Fully working manager implemented

--- manage.py
+++ manage.py
@@ -5,28 +5,45 @@ from db import DB
 from crawler import CRAWLER
 from mobile_de import scraper
 
-def process_listings_links(db):
+
+def process_listings_links(db, cr):
     sleep(5)
     while True:
-        urls = db.read_table("listings_links")
+        urls = cr.listings_links
         for url in urls:
             car_data, db_name = scraper.get_car_data(url + "&lang=en", find_db=True)
             db.add_value(db_name, car_data)
+            cr.listings_links.remove(url)
+            if not cr.running:
+                break
+        if not cr.running:
+            break
         sleep(5)
 
 
 if __name__ == "__main__":
-    db = DB()
+    database = DB()
+    crawlr = CRAWLER(database)
 
-    search_thread = threading.Thread(target=process_listings_links, args=(db,))
+    search_thread = threading.Thread(
+        target=process_listings_links,
+        args=(
+            database,
+            crawlr,
+        ),
+    )
     search_thread.start()
 
-    CR = CRAWLER(db)
-
-    input()
-    while CR.running:
-        sleep(5)
-
-    CR.stop()
-    search_thread.join()
-    os._exit(0)
+    try:
+        input("Type anything to stop execution: ")
+    except KeyboardInterrupt:
+        pass
+    finally:
+        print("Interruption detected, stopping execution")
+        crawlr.stop()
+        print("Crawler stopped")
+        search_thread.join()
+        print("Search thread stopped")
+        database.close_conn()
+        print("Database closed")
+        os._exit(0)
