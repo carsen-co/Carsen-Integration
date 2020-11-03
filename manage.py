168ab3f725b994178b92f131c694ae73862a128f
Mark Melnic <markhappy200@gmail.com>
2020-11-03 09:48:08 +0100

    Added managing file (basically main)

--- manage.py
+++ manage.py
@@ -1,31 +1,32 @@
-import os
+import os, sqlite3, threading
 from time import sleep
-from threading import Thread
-import db, crawler, mobile_de
 
-def process_listings_links():
+from db import DB
+from crawler import CRAWLER
+from mobile_de import scraper
+
+def process_listings_links(db):
     sleep(5)
     while True:
-        try:
-            url = crawler.listing()
-            print(url)
-            car_data, db_name = mobile_de.get_car_data(url, find_db=True)
-            print(db_name, car_data[1:])
+        urls = db.read_table("listings_links")
+        for url in urls:
+            car_data, db_name = scraper.get_car_data(url + "&lang=en", find_db=True)
             db.add_value(db_name, car_data)
-        except Exception as e:
-            sleep(5)
-            print(e)
+        sleep(5)
+
 
 if __name__ == "__main__":
-    database = db.DB()
+    db = DB()
 
-    search_thread = Thread(target=process_listings_links, args=())
+    search_thread = threading.Thread(target=process_listings_links, args=(db,))
     search_thread.start()
 
-    try:
-        crawler = crawler.CRAWLER(database)
+    CR = CRAWLER(db)
 
-    except KeyboardInterrupt:
-        crawler.stop()
-        search_thread.join()
-        os._exit(0)
+    input()
+    while CR.running:
+        sleep(5)
+
+    CR.stop()
+    search_thread.join()
+    os._exit(0)
